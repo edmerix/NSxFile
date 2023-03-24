8e39580a9d55d189c164c60a3d2bc9775f1a4d8a
edmerix <teddymerricks@hotmail.com>
2023-03-23 20:47:14 -0400

    Fixing bug when too few spikes detected for export to UMS2k

--- NSxFile.m
+++ NSxFile.m
@@ -372,25 +372,29 @@ classdef (CaseInsensitiveProperties=true) NSxFile < handle
                 if obj.spikes(settings.channels(c)).loaded
                     ind = settings.channels(c);
                     count = length(obj.spikes(ind).spiketimes);
-                    spikes{c} = ss_default_params(obj.Fs);
-                    
-                    spikes{c}.info.channel = ind;
-        
-                    spikes{c}.info.detect.stds = obj.spikes(ind).sd;
-                    spikes{c}.info.detect.dur = obj.spikes(ind).duration;
-                    spikes{c}.info.detect.thresh = obj.spikes(ind).threshold;
-                    spikes{c}.info.detect.align_sample = floor(-obj.spikes(ind).window(1)*(obj.Fs/1e3)) + 1;
-                    spikes{c}.info.detect.event_channel = obj.spikes(ind).channel * ones(1,count);
+                    if count < 2
+                        disp([9 9 'Skipping ' num2str(settings.channels(c)) ': too few spikes'])
+                    else
+                        spikes{c} = ss_default_params(obj.Fs);
 
-                    spikes{c}.waveforms = obj.spikes(ind).waveforms;
-                    spikes{c}.spiketimes = obj.spikes(ind).spiketimes;
-                    spikes{c}.trials = ones(1,count);
-                    spikes{c}.unwrapped_times = obj.spikes(ind).spiketimes;
+                        spikes{c}.info.channel = ind;
 
-                    [pca.u,pca.s,pca.v] = svd(detrend(spikes{c}.waveforms(:,:),'constant'), 0);
-                    spikes{c}.info.pca = pca;
-                    spikes{c}.info.detect.cov = obj.spikes(ind).covariance;
-                    spikes{c}.info.align.aligned = 1; % this detection method is always aligned
+                        spikes{c}.info.detect.stds = obj.spikes(ind).sd;
+                        spikes{c}.info.detect.dur = obj.spikes(ind).duration;
+                        spikes{c}.info.detect.thresh = obj.spikes(ind).threshold;
+                        spikes{c}.info.detect.align_sample = floor(-obj.spikes(ind).window(1)*(obj.Fs/1e3)) + 1;
+                        spikes{c}.info.detect.event_channel = obj.spikes(ind).channel * ones(1,count);
+
+                        spikes{c}.waveforms = obj.spikes(ind).waveforms;
+                        spikes{c}.spiketimes = obj.spikes(ind).spiketimes;
+                        spikes{c}.trials = ones(1,count);
+                        spikes{c}.unwrapped_times = obj.spikes(ind).spiketimes;
+
+                        [pca.u,pca.s,pca.v] = svd(detrend(spikes{c}.waveforms(:,:),'constant'), 0);
+                        spikes{c}.info.pca = pca;
+                        spikes{c}.info.detect.cov = obj.spikes(ind).covariance;
+                        spikes{c}.info.align.aligned = 1; % this detection method is always aligned
+                    end
                 end
             end
             %spikes(dropping == 1) = [];
